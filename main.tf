4d31c3d6668302d33a582a50cc9a6166d53320de
James Deane <jdeane@cloudflare.com>
2022-05-06 16:50:51 +1000

    init

---
+++ main.tf
@@ -0,0 +1,20 @@
+variable "cloudflare_account_id" {}
+variable "cloudflare_api_key" {}
+variable "cloudflare_domain" {}
+variable "cloudflare_email" {}
+variable "cloudflare_zone_id" {}
+
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  email = var.cloudflare_email
+  api_key = var.cloudflare_api_key
+  account_id = var.cloudflare_account_id
+}
